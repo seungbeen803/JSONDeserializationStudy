3a838d2f52e20a2dfb49e3880ca249ddd114ffbc
Mirim <w2126@e-mirim.hs.kr>
2022-07-27 10:47:58 +0900

    7월 27일 수업

--- app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
+++ app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.jsondeserializationstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 data class MyJSONDataClass(
     val data1: Int,
     val data2: String,
     val list: List<Int>)
+
+data class MyJOSNDataClass(val nested: MyJSONDataClass)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +26,21 @@ class MainActivity : AppCompatActivity() {
                 "list": [1, 2, 3]
             }
         """.trimIndent()
+
+        val d1 = mapper?.readValue<MyJSONDataClass>(jsonString)
+        Log.d("mytag", d1.toString())
+
+        val jsonString2 = """
+            {
+                "nested": {
+                    "data1": 1234,
+                    "data2": "Hello",
+                    "list": [1, 2, 3]
+                    }
+            }
+        """.trimIndent()
+
+        val d2 = mapper?.readValue<MyJOSNDataClass>(jsonString2)
+        Log.d("mytag", d2.toString())
     }
 }
