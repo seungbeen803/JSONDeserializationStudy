8a338b6cba5e964392717723b26ab1099a5b864e
Mirim <w2126@e-mirim.hs.kr>
2022-07-27 10:34:05 +0900

    7월 27일 수업

--- app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
+++ app/src/main/java/com/example/jsondeserializationstudy/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.jsondeserializationstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
+data class MyJSONDataClass(
+    val data1: Int,
+    val data2: String,
+    val list: List<Int>)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var mapper = jacksonObjectMapper()
+        val jsonString = """
+            {
+                "data1": 1234,
+                "data2": "Hello",
+                "list": [1, 2, 3]
+            }
+        """.trimIndent()
     }
 }
